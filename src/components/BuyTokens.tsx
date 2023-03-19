c025654a574b2dc738f883fba2093eff829e17da
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-18 19:32:46 -0600

    Working on buy tokens

--- src/components/BuyTokens.tsx
+++ src/components/BuyTokens.tsx
@@ -7,14 +7,10 @@ import lotteryJson from '../assets/Lottery.json';
 
 export const BuyTokens = () => {
     const { address, isConnected, isDisconnected } = useAccount()
-    // const contract = useContract({
-    //     address: '0xdaD7677997871308ab84E22C93A6231cAe0B67f3',
-    //     abi: lotteryJson.abi,
-    //   })
     const { config } = usePrepareContractWrite({
     address: '0xdaD7677997871308ab84E22C93A6231cAe0B67f3',
     abi: lotteryJson.abi,
-    functionName: 'feed',
+    functionName: 'purchaseTokens',
     })
     const { data, isLoading, isSuccess, write } = useContractWrite(config)
       
@@ -22,10 +18,14 @@ export const BuyTokens = () => {
         return (
             <Card sx={{ minWidth: 275 }}>
                 <CardContent>
-                <Typography component={'span'} variant={'body1'} align={'center'}>
-                    PLACEHOLDER - BUY TOKENS
-                    // usePrepareSendTransaction
-                    // useSendTransaction
+                    <Typography component={'span'} variant={'body1'} align={'center'}>
+                        <div>
+                            <button disabled={!write} onClick={() => write?.()}>
+                                Purchase Tokens
+                            </button>
+                            {isLoading && <div>Check Wallet</div>}
+                            {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+                        </div>
                     </Typography>
                 </CardContent>
             </Card>
