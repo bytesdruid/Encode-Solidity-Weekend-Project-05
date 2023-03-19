a42d1b793967ceb3a33d6dcc09ad8f36d13297ec
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-18 22:38:05 -0600

    Working on return tokens

--- src/components/ReturnTokens.tsx
+++ src/components/ReturnTokens.tsx
@@ -2,29 +2,29 @@ import { useAccount, useContract, usePrepareContractWrite, useContractWrite } fr
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { BigNumber, ethers, providers, Signer, utils } from 'ethers';
+import { BigNumber, ethers, providers, Signer } from 'ethers';
 import lotteryJson from '../assets/Lottery.json';
 import React from 'react';
 import { parseBytes32String } from 'ethers/lib/utils.js';
 
 export const ReturnTokens = () => {
-    const [closingTime, setClosingTime] = React.useState("0")
-    const closingTimeNumber = utils.parseEther(closingTime)
-    // const closingTimeConversion = closingTimeNumber.div(1000000000000000000)
+    const [amount, setAmount] = React.useState("0")
     const { address, isConnected, isDisconnected } = useAccount()
+    // const formatAmount = parseBytes32String(amount)
+    const formatEtherAmount = ethers.utils.parseEther(amount)
     const { config } = usePrepareContractWrite({
         address: '0xdaD7677997871308ab84E22C93A6231cAe0B67f3',
         abi: [
             {
-              name: 'openBets',
+              name: 'returnTokens',
               type: 'function',
               stateMutability: 'nonpayable',
-              inputs: [{ internalType: 'uint256', name: 'closingTime', type: 'uint256' }],
+              inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
               outputs: [],
             },
         ],
-        functionName: 'openBets',
-        args: [closingTimeNumber],
+        functionName: 'returnTokens',
+        args: [formatEtherAmount],
     })
     const { data, isLoading, isSuccess, write } = useContractWrite(config)
       
@@ -33,20 +33,20 @@ export const ReturnTokens = () => {
             <Card sx={{ minWidth: 275 }}>
                 <CardContent>
                     <Typography component={'span'} variant={'body1'} align={'center'}>
-                        ReturnTokens Component - Returns eth and burns tokens.
+                        ReturnTokens Component - Burn tokens and get ether back.
                         <div>
                             <button disabled={!write} onClick={() => write?.()}>
-                                Open Bets
+                                Return Tokens
                             </button>
                             {isLoading && <div>Check Wallet</div>}
                             {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
                         </div>
                         <div>
                             <input
-                                id="closingBlockNumber"
-                                onChange={(e) => setClosingTime(e.target.value)}
-                                placeholder="Closing block number."
-                                value={closingTime}
+                                id="amount"
+                                onChange={(e) => setAmount(e.target.value)}
+                                placeholder="Amount of tokens to burn."
+                                value={amount}
                             />
                         </div>
                     </Typography>
